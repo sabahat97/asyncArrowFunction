4043c61ec12b46f2f92b43aa673d24a7774fad2f
syeda <sbtshakeel61@gmail.com>
2024-06-18 19:37:43 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let arrow = async () => {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res("Hello Arrow Function");
+            rej("Hello General Function");
+        }, 2000);
+    });
+};
+let response = await arrow();
+console.log(response);
+export {};
